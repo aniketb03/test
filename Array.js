415e00e413ac70453cf4a94db6e8e2adf0f71faf
aniketb03 <apbagul03@gmail.com>
2022-04-25 11:41:16 +0530

    First commit

---
+++ Array.js
@@ -0,0 +1,8 @@
+let array=["Aniket","Bagul","Male",23,"Nashik"];
+let[Firstname,Lastname,Gender,Age,Location]=array;
+console.log(`First NAme:${Firstname}
+             Second Name:${Lastname}
+             Gender:${Gender};
+             Age:${Age}
+             Location:${Location}
+`)
